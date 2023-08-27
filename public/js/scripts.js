a347aefa5573377e4caa3a94280f559df823344a
Sarbani Pal <palsarbani2002@gmail.com>
2023-08-27 18:24:52 +0530

    Adding GoldenProject files

---
+++ public/js/scripts.js
@@ -0,0 +1,6 @@
+/*!
+* Start Bootstrap - Small Business v5.0.5 (https://startbootstrap.com/template/small-business)
+* Copyright 2013-2022 Start Bootstrap
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
